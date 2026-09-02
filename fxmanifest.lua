c5b3cba3faee3301f4b58c368f75b6295883f75c
`Baxxy <hesam.seyediahmadi@gmail.com>
2026-09-02 07:57:30 +0330

    Update fxmanifest.lua description formatting

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,6 @@
 fx_version 'cerulean'
 game 'gta5'
-description 'QB-Scoreboard Redesigned by Didax19'
+description 'QB-Scoreboard Redesigned'
 version '1.0.0'
 ui_page 'html/ui.html'
 shared_script 'config.lua'
